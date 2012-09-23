4a1ca3219bec0ca43aba7a9ce4f30e58fcc4d044
Claudius Coenen <coenen@meso.net>
2012-09-23 21:51:39 +0200

    initial commit with empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinMI::Application.config.secret_token = '812dcc3a121c9d617a53982dc781496c679e8952e84f7d7a2df595598ae43d0d2bbbd1b57c546505e7dadbaf1191c8fb0677878054f20b458fe3b9030607232f'
